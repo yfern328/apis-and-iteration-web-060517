3ca171dbe1b37e22ed32538b818b27dc13c5f904
yfern328 <yfernan2@u.rochester.edu>
2017-06-07 17:52:31 -0400

    finished Star Wars lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,9 +6,17 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  # binding.pry
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+
+  film_urls = character_hash["results"].each_with_object([]) do |character_trait_hash, film_url|
+    if character_trait_hash["name"] == character
+      film_url << character_trait_hash["films"]
+    end
+  end.flatten
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -16,10 +24,37 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  film_data = film_urls.each_with_object([]) do |url, film_info|
+    film_info << JSON.parse(RestClient.get(url))
+  end
+
 end
 
-def parse_character_movies(films_hash)
+#DO THIS LATER FOR PROVIDING LIST OF CHARACTERS FOR USER
+# def get_all_characters
+#   character_hash
+# end
+
+def parse_character_movies(films_array)
   # some iteration magic and puts out the movies in a nice list
+  # film_titles = []
+  # films_array.each do |film_dat|
+  #   film_titles = film_dat.each_with_object([]) do |(attribute, info), titles|
+  #       #if attribute == "title"
+  #         titles << film_dat["title"]
+  #     end
+  # end
+
+  films_array.each do |hash|
+    hash.each do |attribute, information|
+      puts "#{attribute.capitalize} : #{information}"
+      puts "\n"
+    end
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  end
+  #film_titles
+
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,15 @@
+
+
 def welcome
   # puts out a welcome message here!
+  puts "Looks like you want some information about a Star Wars character..."
+  puts "Let me help"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  #puts "Please select a character from the following list:"
+  puts "Please type in the name of a character:"
+  #p all_characters
   # use gets to capture the user's input. This method should return that input, downcased.
+  user_input = gets.chomp
 end
